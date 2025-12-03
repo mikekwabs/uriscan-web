7cd933574a85ca8758b5f0c30c60c8e8adf22c27
Michael Koranteng <mikekobby10@gmail.com>
2025-12-03 14:23:05 +0000

    chore: added URIT reading column :dart:

--- app/pages/1_Submissions.py
+++ app/pages/1_Submissions.py
@@ -115,16 +115,17 @@ def show_submission_detail(submission_id: str):
 
             #Convert both to dict for comparison
             uriscan_dict = {r["parameter"]: r["selectedValue"] for r in uriscan_results }
-            # lab_manual_strip_dict = {r["parameter"]: r["selectedValue"] for r in lab_manual_strip_results } 
+            lab_manual_strip_dict = {r["parameter"]: r["selectedValue"] for r in lab_manual_strip_results } 
             analyzer_results_dict = {r["parameter"]: r["selectedValue"] for r in analyzer_results } if analyzer_results else {}
 
             data = []
             for param, uriscan_val in uriscan_dict.items():
-                # lab_manual_test_val = lab_manual_strip_dict.get(param, "-")
+                lab_manual_test_val = lab_manual_strip_dict.get(param, "-")
                 analyzer_val = analyzer_results_dict.get(param, "-")
                 data.append({
                     "Parameter": param,
                     "URS-14EA Reading": uriscan_val,
+                    "URIT Reading": lab_manual_test_val,
                     "MEDITAPE UC-11A Reading": analyzer_val,
                 })
             df = pd.DataFrame(data)
